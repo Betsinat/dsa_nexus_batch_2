5ea25451220005a81fffc88f5be7b53607458c41
Betsinat <Betsinat39@gmail.com>
2025-07-16 17:23:19 +0300

    Create Goal Parser interprtation.py

---
+++ Goal Parser interprtation.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def interpret(self, command):
+        i = 0
+        res =""
+        while i < len(command):
+            if command[i] == 'G':
+              res +="G"
+              i +=1
+            elif command[i:i+2] == "()":
+                res += "o"
+                i +=2
+            else:
+                res +="al"
+                i+=4
+        return res
